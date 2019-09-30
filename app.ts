5e125f0e707a88dba6fd7f54a6270e0e4589c7ef
KristinaStepanova <kri18stina@gmail.com>
2019-09-30 20:55:20 +0300

    hw

---
+++ app.ts
@@ -0,0 +1,37 @@
+//task 1
+function checkPalindrome(originStr: string): boolean {
+  if(typeof originStr !== "string") {
+    console.error("Incorrect type");
+    return false;
+  }
+  let reverseStr: string = originStr
+    .split("")
+    .reverse()
+    .join("");
+  if (originStr !== reverseStr) {
+    return false;
+  }
+  return true;
+}
+
+console.log(checkPalindrome("nan"));
+
+//task 2
+function getFibonacci(userNum: number): Array<number> {
+  if(typeof userNum !== "number" || userNum <= 0) {
+    console.error("Incorrect input");
+    return [];
+  }
+  let fibArr: Array<number> = [0, 1];
+
+  let fibNumber: number = 0;
+
+  while (fibNumber <= userNum - fibArr[fibArr.length - 1]) {
+    fibNumber = fibArr[fibArr.length - 1] + fibArr[fibArr.length - 2];
+    fibArr.push(fibNumber);
+    console.log(fibNumber);
+  }
+  return fibArr;
+}
+
+console.log(getFibonacci(22));
