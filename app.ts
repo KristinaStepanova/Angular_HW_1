a38478ed188c5202f325403c06eace5ad41bd6fa
KristinaStepanova <kri18stina@gmail.com>
2019-10-01 15:01:00 +0300

    task 2

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 //task 1
 function checkPalindrome(originStr: string): boolean {
-  if(typeof originStr !== "string") {
+  if (typeof originStr !== "string") {
     console.error("Incorrect type");
     return false;
   }
@@ -18,20 +18,21 @@ console.log(checkPalindrome("nan"));
 
 //task 2
 function getFibonacci(userNum: number): Array<number> {
-  if(typeof userNum !== "number" || userNum <= 0) {
+  if (typeof userNum !== "number" || userNum <= 0) {
     console.error("Incorrect input");
     return [];
   }
+
   let fibArr: Array<number> = [0, 1];
 
   let fibNumber: number = 0;
 
-  while (fibNumber <= userNum - fibArr[fibArr.length - 1]) {
+  while (fibNumber < userNum) {
     fibNumber = fibArr[fibArr.length - 1] + fibArr[fibArr.length - 2];
     fibArr.push(fibNumber);
-    console.log(fibNumber);
   }
-  return fibArr;
+
+  return fibArr.slice(0, -1);
 }
 
-console.log(getFibonacci(22));
+console.log(getFibonacci(90));
